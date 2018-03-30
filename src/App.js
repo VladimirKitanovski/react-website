91b643c944fd03ddab9e2bcbeeddd46e33ae1ea3
Vladimir <vladimirkitanovski22@gmail.com>
2018-03-30 20:56:48 +0200

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+// components
+import Header from "./component/headerComponent/header";
+import Footer from "./component/footerComponent/footer";
+import Homepage from "./component/pages/homePage";
+
+// includes
+import './Assets/css/scss/default.min.css';
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
 
+      <Homepage />
+
+      <Footer />
       </div>
     );
   }
